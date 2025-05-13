964db366f7cdec9b633d26b30ccfdf855459ae03
Kos Tsvilii <tsvilii.dev@gmail.com>
2025-05-13 16:03:58 -0500

    config setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
 export default defineConfig({
   plugins: [tailwindcss(), tsconfigPaths()],
   server: {
     open: true,
+  },
+  build: {
+    outDir: 'dist',
   },
 
   resolve: {
